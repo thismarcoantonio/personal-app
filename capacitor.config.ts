3c38288f28da4af8ecc51439f6cb01699d433fec
thismarcoantonio <thismarcoantonio@gmail.com>
2023-02-11 14:13:42 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.personalapp.com',
+  appName: 'Personal App',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
