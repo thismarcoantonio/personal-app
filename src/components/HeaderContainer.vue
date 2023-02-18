eba8196af0eb0cb0347ed8c36dd92079e42d599f
thismarcoantonio <thismarcoantonio@gmail.com>
2023-02-18 15:26:59 -0300

    Add PageHeader component

--- src/components/HeaderContainer.vue
+++ src/components/HeaderContainer.vue
@@ -1,7 +1,7 @@
 <template>
   <header
-    :class="{ 'px-6': !noGutters }"
-    class="py-8 bg-primary-500 text-white rounded-bl-2xl overflow-hidden"
+    :class="{ 'px-6': !noGutters, 'py-8': !small, 'py-6': small }"
+    class="bg-primary-500 text-white rounded-bl-2xl overflow-hidden"
   >
     <slot />
   </header>
@@ -10,5 +10,6 @@
 <script lang="ts" setup>
 defineProps<{
   noGutters?: boolean;
+  small?: boolean;
 }>();
 </script>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="text-xl text-white font-bold">
+    <slot>{{ title }}</slot>
+  </h1>
+  <p class="text-sm text-gray-200" v-if="description">{{ description }}</p>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  title: string;
+  description?: string;
+}>();
+</script>
