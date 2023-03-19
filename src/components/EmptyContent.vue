f5b2934a5af93b6b0969200a79b09a83ab801009
thismarcoantonio <thismarcoantonio@gmail.com>
2023-03-19 15:30:40 -0300

    Add empty state to reminders page

---
+++ src/components/EmptyContent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col items-center text-center mt-4">
+    <div class="text-lg text-gray-700 font-semibold mb-2">{{ title }}</div>
+    <p class="text-sm text-gray-500 mb-4">{{ description }}</p>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  title: string;
+  description: string;
+}>();
+</script>
--- src/views/Reminders/RemindersView.vue
+++ src/views/Reminders/RemindersView.vue
@@ -1,22 +1,27 @@
 <template>
-  <page-header
-    title="Reminders"
-    description="Calendar view"
-    @create="isFullscreenOpen = true"
-  >
+  <page-header title="Reminders" description="Calendar view">
     <template #suffix>
       <icon-button @click="isFullscreenOpen = true" icon="add" rounded />
     </template>
   </page-header>
-  <ul class="p-4 pb-20">
-    <info-card
-      is="li"
-      v-for="reminder in reminders"
-      :key="reminder.id"
-      :title="reminder.title"
-      :description="`${reminder.date}`"
-    />
-  </ul>
+  <div class="p-4 pb-20">
+    <empty-content
+      v-if="!reminders.length"
+      title="No reminders found"
+      description="Create import reminders of your daily life, so you can be notified about it"
+    >
+      <main-button @click="isFullscreenOpen = true">Add reminder</main-button>
+    </empty-content>
+    <ul v-else>
+      <info-card
+        is="li"
+        v-for="reminder in reminders"
+        :key="reminder.id"
+        :title="reminder.title"
+        :description="`${reminder.date}`"
+      />
+    </ul>
+  </div>
   <fullscreen-dialog title="New reminder" v-model:open="isFullscreenOpen">
     <form-wrapper @submit="handleSubmit" v-slot="form">
       <text-field name="title" label="Title" required />
@@ -47,6 +52,7 @@ import { ref, onMounted } from "vue";
 import { getReminders, createReminder, type Reminder } from "@/database";
 import PageHeader from "@/components/PageHeader.vue";
 import IconButton from "@/components/IconButton.vue";
+import EmptyContent from "@/components/EmptyContent.vue";
 import FullscreenDialog from "@/components/FullscreenDialog.vue";
 import FormWrapper from "@/components/Form/FormWrapper.vue";
 import TextField from "@/components/Form/TextField.vue";
@@ -57,7 +63,7 @@ import MainButton from "@/components/MainButton.vue";
 import InfoCard from "@/components/InfoCard.vue";
 
 const isFullscreenOpen = ref(false);
-const reminders = ref<Reminder[]>();
+const reminders = ref<Reminder[]>([]);
 
 const frequencyOptions = [
   { value: "once", label: "Once" },
